39eb0b4a269ab235706d6c23b35a2868bf213c78
neo <139451447+yonathanjj@users.noreply.github.com>
2024-11-02 18:52:41 +0300

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    hamburger.addEventListener("click", function () {
+        mobileMenu.classList.toggle("hidden");
+    });
+});
